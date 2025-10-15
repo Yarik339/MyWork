4c81c3d39b7f21f92a35826338b35902619bc1ac
Daniil <danil.grytsenko12@gmail.com>
2025-10-15 17:14:56 +0300

    addition, subtraction, multiplication, and division

---
+++ src/Basics.java
@@ -0,0 +1,16 @@
+public class Basics {
+    public static void main(String[] args){
+        int age = 19;
+        double height = 1.78;
+        String name = "Daniil";
+        boolean alive = true;
+        double plus = age + height;
+        System.out.println(plus);
+        double minus = age - height;
+        System.out.println(minus);
+        double multiplication = age * height;
+        System.out.println(multiplication);
+        double division = age / height;
+        System.out.println(division);
+    }
+}
